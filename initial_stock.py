f48c01b943e323c8045909e9174e9805fe102cc6
sonofander <dcoltonanderson@gmail.com>
2017-10-09 15:59:12 -0500

    Updated charts

--- initial_stock.py
+++ initial_stock.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import pandas_datareader.data as web
 import datetime
+import matplotlib.pyplot as plt
+%matplotlib inline
+%pylab inline
 
 
 start = datetime.datetime(2016,6,1)
@@ -8,3 +11,10 @@ end = datetime.datetime.today()
 uwt = web.DataReader('UWT', "google", start, end)
 
 print type(uwt)
+
+
+pylab.rcParams['figure.figsize'] = (15,9)
+
+uwt["Adj Close"].plot(grid = True)
+
+#candlestick plots
